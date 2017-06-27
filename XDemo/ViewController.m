f4a71ee94880240165f72ccb832c0155a87f869e
sajiner <739134362@qq.com>
2017-06-27 14:11:09 +0800

    NSCache

--- XDemo/ViewController.m
+++ XDemo/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSCacheDelegate>
 
 @property (nonatomic, strong) NSCache *cache;
 
@@ -20,19 +20,45 @@
     [super viewDidLoad];
     
     
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (int i = 0; i < 20; i++) {
+        NSString *str = [NSString stringWithFormat:@"hello -- %d", i];
+        
+        NSLog(@"设置===%@", str);
+        [self.cache setObject:str forKey:@(i)];
+    }
+    
+    for (int i = 0; i < 20; i++) {
+        NSLog(@"%@", [self.cache objectForKey:@(i)]);
+    }
     
 }
 
+#pragma mark - NSCacheDelegate
+/*
+ 当缓存中的对象被清除的时候，会自动调用
+ 满10个后，会先清理再添加
+ 2017-06-27 14:03:59.529 XDemo[1162:28917] 将要清理：hello -- 0
+ 2017-06-27 14:03:59.529 XDemo[1162:28917] 设置===hello -- 11
+ 2017-06-27 14:03:59.529 XDemo[1162:28917] 将要清理：hello -- 1
+ 2017-06-27 14:03:59.529 XDemo[1162:28917] 设置===hello -- 12
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+ */
+- (void)cache:(NSCache *)cache willEvictObject:(id)obj {
+    // 会影响清理速度，所以，平时开发的时候不建议重写此代理方法；仅供调试时使用
+    [NSThread sleepForTimeInterval:1];
+    NSLog(@"将要清理：%@", obj);
 }
-
 
 - (NSCache *)cache {
     if (!_cache) {
         _cache = [[NSCache alloc] init];
+        // 设置数量限制
+        _cache.countLimit = 10;
+        _cache.delegate = self;
     }
     return _cache;
 }
