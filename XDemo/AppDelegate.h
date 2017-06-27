7d96d998e222c624c4a1a3df0113865a8c22a679
sajiner <739134362@qq.com>
2017-06-27 13:26:18 +0800

    Initial Commit

---
+++ XDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDemo
+//
+//  Created by 张鑫 on 2017/6/27.
+//  Copyright © 2017年 张鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDemo
+//
+//  Created by 张鑫 on 2017/6/27.
+//  Copyright © 2017年 张鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
